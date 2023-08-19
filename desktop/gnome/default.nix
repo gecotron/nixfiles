751b3f6b3241e42dd4d1987f712859fa036e6699
Michael Ward <gecotron@gmail.com>
2023-08-18 21:16:33 -0400

    hoping and praying

--- desktop/gnome/default.nix
+++ desktop/gnome/default.nix
@@ -2,7 +2,6 @@
 
 {
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Install extensions
