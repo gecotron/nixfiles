a9885a960df5796dff3e39adcda102151d625ab8
Michael Ward <gecotron@gmail.com>
2023-08-19 20:40:34 -0400

    nvidia nonsense

--- desktop/default.nix
+++ desktop/default.nix
@@ -4,6 +4,7 @@
   # Import wm configs
   imports = [
     # ./berrywm
+    ./nvidia.nix
     ./gnome
     # ./hyprland
   ];
@@ -15,28 +16,6 @@
     logFile = "/var/log/Xorg.0.log";
   };
 
-  # Configure nvidia GPU drivers
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    nvidiaSettings = true;
-    # Set up prime
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-
-      intelBusId = "PCI:00:02:0";
-      nvidiaBusId = "PCI:01:00:0";
-    };
-  };
-  
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
@@ -45,9 +24,8 @@
 
   # Enable display manager
   services.xserver.displayManager = {
-    gdm.enable = false;
+    gdm.enable = true;
     gdm.wayland = false;
-    sddm.enable = true;
     defaultSession = "gnome";
   };
   
---
+++ desktop/nvidia.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  let  
+  nv-offload = pkgs.writeShellScriptBin "nvidia-offload" '' 
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+  in {
+  # Configure nvidia GPU drivers
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    # Set up prime
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:00:02:0";
+      nvidiaBusId = "PCI:01:00:0";
+    };
+  };
+
+  environment.systemPackages = [ nv-offload ];
+  }
+}
