451cbd1cd6dbde61902f5b7c002f53a1016cbb3c
Michael Ward <gecotron@gmail.com>
2023-08-22 17:40:13 -0400

    maybe maybe maybe

--- home/mc/default.nix
+++ home/mc/default.nix
@@ -1,15 +1,14 @@
 { config, pkgs, ... }:
 
 let
-  prismlauncherDesktop = pkgs.makeDesktopItem {
-    name = "prismlauncher";
-    desktopName = "Prism Launcher (Nvidia)";
+  prismLauncherDesktop = {
+    name = "Prism Launcher";
+    genericName = "Minecraft Launcher";
+    desktopName = "Prism Launcher";
     exec = ''sh -c "nv-offload prismlauncher"'';
     terminal = false;
   };
 in {
-  home.packages = with pkgs; [
-    prismlauncher
-    prismlauncherDesktop
-  ];
+  home.packages = with pkgs; [ prismlauncher ];
+  xdg.desktopEntries.prismlauncher = prismLauncherDesktop
 }
