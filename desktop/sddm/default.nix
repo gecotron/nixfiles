d253abed676b5f381d44b052ba9e7d4a1dc56dd4
Michael Ward <gecotron@gmail.com>
2023-08-23 12:13:48 -0400

    syntax error

--- desktop/sddm/default.nix
+++ desktop/sddm/default.nix
@@ -3,7 +3,7 @@
 {
   services.xserver.displayManager.sddm.sugarCandyNix = {
     enable = true;
-    Background = lib.cleanSource ./background.jpg
+    Background = lib.cleanSource ./background.jpg;
     ScreenWidth = 1920;
     ScreenHeight = 1080;
     FormPosition = "left";
